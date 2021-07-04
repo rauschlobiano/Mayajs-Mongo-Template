c9050af0fe532342e14a6b27e1f788a12bdbd3a5
Ron Jones <rondfirst@gmail.com>
2021-07-04 22:46:40 +0800

    Implement MongoDb module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { routes } from "./app.routing.module";
 import { AppController } from "./app.controller";
 import { SampleController } from "./controllers/sample/sample.controller";
 import { SampleServices } from "./controllers/sample/sample.service";
+import { MongoDbModule, MongoDbServices } from "@mayajs/mongo";
+import { mongoOptions } from "./database/mongo";
 
 @Module({
   declarations: [AppController, SampleController],
-  imports: [RouterModule.forRoot(routes)],
-  providers: [SampleServices],
+  imports: [RouterModule.forRoot(routes), MongoDbModule.forRoot(mongoOptions)],
+  providers: [SampleServices, MongoDbServices],
   bootstrap: AppController,
 })
 export class AppModule {}
