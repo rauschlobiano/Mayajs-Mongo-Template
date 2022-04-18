7782f47e1c07abb4772d7cf5f1b16db6f6ba0121
Rausch Lobiano <rondfirst@gmail.com>
2022-04-18 23:59:11 +0800

    First Blood

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import { configServer } from "@mayajs/core";
 import { AppModule } from "./app.module";
 
+import bodyParser from "body-parser";
+import cors from "cors";
+import env from "./environments";
+
+var corsOptions = {
+  //origin: "*",
+  origin: [
+    "http://localhost:3334",
+    "https://parishfront-ljvn5.ondigitalocean.app",
+  ],
+  methods: ["GET", "PUT", "POST", "DELETE", "PATCH"],
+  optionsSuccessStatus: 200,
+};
+
 configServer(3333)
   .usePlugins([
-    // Add plugins or middlewares here
-    // i.e. CORS, body-parser, multer and etc...
+    bodyParser.urlencoded({ extended: false }),
+    bodyParser.json(),
+    cors(corsOptions),
   ])
   .bootstrapModule(AppModule);
